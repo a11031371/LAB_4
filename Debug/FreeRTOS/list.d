caa32aafe2085ff29b93cf6f8f57536e28268331
unknown <a11031371@gmail.com>
2022-05-12 13:17:38 +0800

    eos_lab4 first commit

---
+++ Debug/FreeRTOS/list.d
@@ -0,0 +1,25 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ C:/RTOS_workspace/LAB_4/FreeRTOS/include/FreeRTOS.h \
+ C:/RTOS_workspace/LAB_4/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/RTOS_workspace/LAB_4/FreeRTOS/include/projdefs.h \
+ C:/RTOS_workspace/LAB_4/FreeRTOS/include/portable.h \
+ C:/RTOS_workspace/LAB_4/FreeRTOS/include/deprecated_definitions.h \
+ C:/RTOS_workspace/LAB_4/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/RTOS_workspace/LAB_4/FreeRTOS/include/mpu_wrappers.h \
+ C:/RTOS_workspace/LAB_4/FreeRTOS/include/list.h
+
+C:/RTOS_workspace/LAB_4/FreeRTOS/include/FreeRTOS.h:
+
+C:/RTOS_workspace/LAB_4/FreeRTOS/include/FreeRTOSConfig.h:
+
+C:/RTOS_workspace/LAB_4/FreeRTOS/include/projdefs.h:
+
+C:/RTOS_workspace/LAB_4/FreeRTOS/include/portable.h:
+
+C:/RTOS_workspace/LAB_4/FreeRTOS/include/deprecated_definitions.h:
+
+C:/RTOS_workspace/LAB_4/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+C:/RTOS_workspace/LAB_4/FreeRTOS/include/mpu_wrappers.h:
+
+C:/RTOS_workspace/LAB_4/FreeRTOS/include/list.h:
---
+++ Debug/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,28 @@
+FreeRTOS/portable/MemMang/heap_4.o: ../FreeRTOS/portable/MemMang/heap_4.c \
+ D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/FreeRTOS.h \
+ D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/FreeRTOSConfig.h \
+ D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/projdefs.h \
+ D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/portable.h \
+ D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/deprecated_definitions.h \
+ D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/mpu_wrappers.h \
+ D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/task.h \
+ D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/list.h
+
+D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/FreeRTOS.h:
+
+D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/FreeRTOSConfig.h:
+
+D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/projdefs.h:
+
+D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/portable.h:
+
+D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/deprecated_definitions.h:
+
+D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/mpu_wrappers.h:
+
+D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/task.h:
+
+D:/RTOS_workspace_ChengKung/LAB_4/FreeRTOS/include/list.h:
